16e26dd61306b9a9bb5d0063d567decbf6d6eadc
ahmura <antonio.hmura@gmail.com>
2018-04-09 13:40:55 +0200

    cetvrti task

--- Begin/ContactManager/Controllers/ContactController.cs
+++ Begin/ContactManager/Controllers/ContactController.cs
@@ -1,6 +1,7 @@
 ﻿using ContactManager.Models;
 using System;
 using System.Collections.Generic;
+using ContactManager.Services;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -10,21 +11,16 @@ namespace ContactManager.Controllers
 {
     public class ContactController : ApiController
     {
+        private ContactRepository contactRepository;
+
+        public ContactController()
+        {
+            this.contactRepository = new ContactRepository();
+        }
+
         public Contact[] Get()
         {
-            return new Contact[]
-            {
-                new Contact
-                {
-                    Id = 1,
-                    Name = "Glenn Block"
-                },
-                new Contact
-                {
-                    Id = 2,
-                    Name = "Dan Roth"
-                }
-            };
+            return this.contactRepository.GetAllContacts();
         }
     }
 }
